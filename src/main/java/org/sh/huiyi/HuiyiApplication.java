29b61aa7f13ebd4415bf025a6f8b6b0856b98227
milan <nothingbutdreams@protonmail.com>
2024-07-04 00:48:14 +0800

    初始化

---
+++ src/main/java/org/sh/huiyi/HuiyiApplication.java
@@ -0,0 +1,13 @@
+package org.sh.huiyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuiyiApplication.class, args);
+	}
+
+}
